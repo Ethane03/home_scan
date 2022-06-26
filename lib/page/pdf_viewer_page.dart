6b4452db7d0592e4372f10670154579346d10cd6
Ethane03 <ethan03@tjfmail.com>
2022-06-26 18:26:43 -0500

    init

---
+++ lib/page/pdf_viewer_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:io' as io;
+
+class PDFViewerPage extends StatefulWidget {
+  final io.File file;
+
+  const PDFViewerPage({
+    Key? key,
+    required this.file,
+  }) : super (key: key);
+
+  @override
+  _PDFViewerPageState createState() => _PDFViewerPageState();
+}
+
+class _PDFViewerPageState extends State<PDFViewerPage> {
+  @override
+  Widget build(BuildContext context){
+    final name = basename(widget.file.path);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name),
+      ),
+      body: PDFView(
+        filePath: widget.file.path,
+      ),
+    );
+  }
+}
